29985cc01fb2d8aa4ba84b6e8e5034a8c934da6c
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-02-26 13:19:03 +0100

    feat(hello_world): print environ

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#ifdef __linux__
+extern char **environ;
+#endif
 
 int main(int argc, char *argv[]) {
     printf("argc = %d\n", argc);
@@ -6,4 +11,11 @@ int main(int argc, char *argv[]) {
         printf("argv[%d] --> %s\n", ndx, argv[ndx]);
     }
     printf("argv[argc] = %p\n", (void *)argv[argc]);
+
+    for (size_t i = 0;; ++i) {
+        printf("environ[%zu] = %s\n", i, environ[i]);
+        if (environ[i] == NULL) {
+            break;
+        }
+    }
 }
